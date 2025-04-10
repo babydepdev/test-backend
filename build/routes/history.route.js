ad504e88de4c587676ccf64ee89ab8abe3260164
babydev <babydev.weangkom@gmail.com>
2025-04-11 00:52:40 +0700

    done

--- build/routes/history.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const history_controller_1 = require("../controllers/history.controller");
-const historyRoutes = (0, express_1.Router)();
-historyRoutes.get("/historys", history_controller_1.getAllOrderHistoryController);
-historyRoutes.get("/history/:userId", history_controller_1.readOrderHistoryByIdController);
-exports.default = historyRoutes;
--- build/routes/product.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const product_controller_1 = require("../controllers/product.controller");
-const productRoutes = (0, express_1.Router)();
-productRoutes.get("/products", product_controller_1.getAllProductController);
-exports.default = productRoutes;
